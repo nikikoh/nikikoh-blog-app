83762c0cb36d207d0acbf13cc1be39ba32f2a7b4
nikikoh <nikikoh5@gmail.com>
2021-01-13 00:13:11 +0900

    feat: unfollow機能を実装しました。

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,12 @@
 class FollowsController < ApplicationController
+<<<<<<< Updated upstream
   def create
     before_action: :authenticate_user!
+=======
+  before_action :authenticate_user!
+
+  def create
+>>>>>>> Stashed changes
     current_user.follow!(params[:account_id])
     redirect_to account_path(params[:account_id])
   end
